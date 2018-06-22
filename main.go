23069eeb8c3749112032fc3c2e9b65c4dc058166
wangzhengjun <wangzhengjun@didichuxing.com>
2018-06-22 19:33:40 +0800

    提交代码

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	_ "net/http/pprof"
+)
+
+//func main() {
+//	go func() {
+//		http.ListenAndServe("localhost:6060", nil)
+//	}()
+//	go_framework.Run()
+//}
+
+func main(){
+	
+}
